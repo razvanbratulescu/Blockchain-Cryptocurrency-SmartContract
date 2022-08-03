79456bde67824f225cb7c47f03ceb2828d213d4f
razvanbratulescu <79872486+razvanbratulescu@users.noreply.github.com>
2022-08-03 11:44:55 +0300

    Update blockchain.py

--- module 1 - create a Blockchain/blockchain.py
+++ module 1 - create a Blockchain/blockchain.py
@@ -35,7 +35,7 @@ class Blockchain:
         new_proof = 1
         check_proof = False
         while check_proof is False:
-            hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexidigest()
+            hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
             if hash_operation[:4] == '0000':
                 check_proof = True
             else:
@@ -56,7 +56,7 @@ class Blockchain:
                 return False
             previous_proof = previous_block['proof']
             proof = block['proof']
-            hash_operation = hashlib.sha256(str(proof**2 - previous_proof**2).encode()).hexidigest()              
+            hash_operation = hashlib.sha256(str(proof**2 - previous_proof**2).encode()).hexdigest()              
             if hash_operation[:4] != '0000':
                 return False
             previous_block = block
